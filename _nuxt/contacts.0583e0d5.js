38da8b4e76fc05861ff7288923cbadeed3fd1190
Smirnov Alexandr <alexrus37reg@gmailcom>
2023-04-20 15:58:51 +0300

    Updates

--- _nuxt/contacts.0583e0d5.js
+++
@@ -1,1 +0,0 @@
-import{a as c,b as t,o as e}from"./entry.33f528de.js";const o={},s={class:"contacts"};function n(a,r){return e(),t("div",s,"Контакты")}const f=c(o,[["render",n]]);export{f as default};
